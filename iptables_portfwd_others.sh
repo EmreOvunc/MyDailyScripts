0e1469a0c19c1d5d1a8c8742149eb53bd966f6eb
Emre <EmreOvunc@users.noreply.github.com>
2018-07-09 00:51:39 +0300

    Update iptables_portfwd_others.sh

--- iptables_portfwd_others.sh
+++ iptables_portfwd_others.sh
@@ -3,5 +3,5 @@ sudo iptables -F
 MY="10.0.1.143"
 DST="10.70.50.1"
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $DST:80
-sudo iptables -t nat -A POSTROUTING -o tap0 -p tcp -d $DST --dport 80 -j SNAT --to-source $MY
+sudo iptables -t nat -A POSTROUTING -p tcp -d $DST --dport 80 -j SNAT --to-source $MY
 sudo sysctl net.ipv4.ip_forward=1
