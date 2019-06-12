618a8663c062a32012e4d4161885b0248ca54319
Emre <EmreOvunc@users.noreply.github.com>
2019-06-12 10:39:38 +0300

    Update getSessiontime.py

--- getSessiontime.py
+++ getSessiontime.py
@@ -11,19 +11,26 @@ from requests import head
 from time import sleep
 
 def threads():
-	for i in range(15):
+	for i in range(10):
 		sleep(0.001)
 		t = Thread(target=getTime)
 		t.start()
 
 def getTime():
-	response = head(url)
-	print(response.headers['Set-Cookie'].split(";")[0])
-	#print(response.headers['Date'])
+	while True:
+		try:
+			response = head(url, cookies=cookies)
+			if (response.headers['Set-Cookie'].split(";")[0][:4]) != "Last":
+				print(response.headers['Set-Cookie'].split(";")[2].split(',')[1].strip())
+			else:
+				print(response.headers['Set-Cookie'].split(";")[0].strip())
+		except:
+			pass
 
 jobs = []
 
 url = "URL"
+cookies = {'sessionID':'testCookie'}
 
 for i in range(5):
     p = Process(target=threads)
