3be5a167ccd15c9d53e0133e812b70c9d79dee78
Emre <EmreOvunc@users.noreply.github.com>
2019-07-24 09:51:17 +0300

    Update internet.sh

--- vpn_gw/internet.sh
+++ vpn_gw/internet.sh
@@ -3,10 +3,10 @@ sudo iptables -F
 sudo sysctl net.ipv4.ip_forward=1
 sudo rm -rf /etc/resolv.conf
 sudo echo "nameserver 1.1.1.1" >> resolv.conf
-sudo echo "nameserver 8.8.8.8" > resolv.conf
+sudo echo "nameserver 8.8.8.8" >> resolv.conf
 sudo echo "nameserver 208.67.222.222" >> resolv.conf
 sudo echo "nameserver 8.8.4.4" >> resolv.conf
-sudo echo "nameserver 208.67.220.220" >> resolv.conf
+sudo echo "nameserver 208.67.222.220" >> resolv.conf
 sudo mv resolv.conf /etc/resolv.conf
 sudo iptables -A FORWARD --in-interface eth0 -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
