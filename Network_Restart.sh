fcb1686f3bd680cee5259d1b6a0db1001ce5cd2b
Emre <EmreOvunc@users.noreply.github.com>
2019-12-16 17:02:43 +0300

    Delete Network_Restart.sh

--- Network_Restart.sh
+++
@@ -1,3 +0,0 @@
-notify-send "Network" "Network Services are restarted!" -i /usr/share/icons/Adwaita/32x32/actions/view-refresh.png
-sudo service networking restart
-sudo service network-manager restart
