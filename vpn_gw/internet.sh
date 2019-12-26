6a580f102fe1323a5fe9d4016ca0afef61554538
Emre <EmreOvunc@users.noreply.github.com>
2019-12-26 09:17:25 +0300

    Update internet.sh

--- vpn_gw/internet.sh
+++ vpn_gw/internet.sh
@@ -2,7 +2,7 @@ sudo iptables -t nat -F
 sudo iptables -F
 sudo sysctl net.ipv4.ip_forward=1
 sudo rm -rf /etc/resolv.conf
-sudo echo "nameserver 1.1.1.1" >> resolv.conf
+sudo echo "nameserver 1.1.1.1" > resolv.conf
 sudo echo "nameserver 8.8.8.8" >> resolv.conf
 sudo echo "nameserver 208.67.222.222" >> resolv.conf
 sudo echo "nameserver 8.8.4.4" >> resolv.conf
